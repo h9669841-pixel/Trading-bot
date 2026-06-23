7b06304b5d19674d7424b1078e52c219ae4da96c
h9669841-pixel <h9669841@gmail.com>
2026-06-23 21:50:11 +0300

    Update GIRIS_MAKAS_YUZDE to 10.45

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
 
 # --- 📊 ARBİTRAJ STRATEJİ VE HESAP AYARLARI ---
-GIRIS_MAKAS_YUZDE = 0.45       # Sadece +%0.45 ve üzerindeki fırsatları avlar
+GIRIS_MAKAS_YUZDE = 10.45       # Sadece +%0.45 ve üzerindeki fırsatları avlar
 CIKIS_MAKAS_YUZDE = 0.10       # Makas +%0.10'un altına daraldığında kârı kilitler ve çıkar
 
 # 25 USDT cüzdan bakiyenizin ucu ucuna sıkışmaması için güvenlik tamponlu bakiye
